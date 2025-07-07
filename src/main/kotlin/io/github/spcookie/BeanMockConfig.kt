1a203fd417d43cc652b9696cdf648e46c09aaebc
咸小鱼 <1307083930@qq.com>
2025-07-07 22:50:03 +0800

    refactor: 将 BeanMockConfig 类的访问修饰符改为 internal
    - 将 data class BeanMockConfig 修改为 internal data class BeanMockConfig
    - 此修改限制了类的可见性，仅在同一个模块内可见
    - 这是一个重构操作，旨在提高代码的封装性和安全性

--- src/main/kotlin/io/github/spcookie/BeanMockConfig.kt
+++ src/main/kotlin/io/github/spcookie/BeanMockConfig.kt
@@ -3,7 +3,7 @@ package io.github.spcookie
 /**
  * Configuration for bean mocking
  */
-data class BeanMockConfig(
+internal data class BeanMockConfig(
     val includePrivate: Boolean = false,
     val includeStatic: Boolean = false,
     val includeTransient: Boolean = false
